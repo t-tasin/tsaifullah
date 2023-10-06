b8c75b582ea2156529212653bc2d0f653abd3578
Tasin <tasin0.net@gmail.com>
2023-10-06 15:22:03 -0400

    Fix: propic and gray ball position

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import ProPic from "./assets/propic.png";
 
 export default function App() {
   return (
-    <Box marginRight={"200px"} marginLeft={"200px"}>
+    <Box
+      marginRight={{ base: 6, lg: "200px" }}
+      marginLeft={{ base: 6, lg: "200px" }}
+    >
       <Flex
         width={"100%"}
         height={"100px"}
@@ -14,8 +17,16 @@ export default function App() {
       >
         <H2 textAlign={"center"}>KM Khalid Saifullah</H2>
       </Flex>
-      <Flex marginTop={24} width={"100%"}>
-        <Box width={"60%"} paddingTop={8} paddingRight={6}>
+      <Flex
+        marginTop={24}
+        width={"100%"}
+        direction={{ base: "column", lg: "row" }}
+      >
+        <Box
+          width={{ base: "100%", lg: "60%" }}
+          paddingTop={8}
+          paddingRight={6}
+        >
           <H1 marginBottom={4}>Hey, This is Tasin</H1>
           <H4>
             I am a Junior at The College of Wooster majoring in Computer Science
@@ -27,22 +38,23 @@ export default function App() {
           </H4>
         </Box>
         <Flex
-          width={"40%"}
+          width={{ base: "100%", lg: "40%" }}
           justifyContent={"center"}
           alignItems={"center"}
-          position={"relative"}
         >
-          <Image src={ProPic} borderRadius="full" boxSize="220px" />
-          <Box
-            borderRadius={"50%"}
-            bgColor={"gray.100"}
-            width={"120px"}
-            height={"120px"}
-            position={"absolute"}
-            bottom={0}
-            right={10}
-            zIndex={-1}
-          />
+          <Box width={"fit-content"} position={"relative"}>
+            <Image src={ProPic} borderRadius="full" boxSize="220px" />
+            <Box
+              borderRadius={"50%"}
+              bgColor={"gray.100"}
+              width={"120px"}
+              height={"120px"}
+              position={"absolute"}
+              bottom={-5}
+              right={-20}
+              zIndex={-1}
+            />
+          </Box>
         </Flex>
       </Flex>
     </Box>
