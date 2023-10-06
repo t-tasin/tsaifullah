03f1d560838627d10a66a101b961dd284bda0113
Tasin <tasin0.net@gmail.com>
2023-10-06 00:56:32 -0400

    Environment fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Flex} from '@chakra-ui/react';
-import { H1 } from './Typography';
+import { Flex } from "@chakra-ui/react";
+import { H1 } from "./Typography";
 
 export default function App() {
   return (
-    <Flex width={'100%'} height={'100px'} bgColor={'gray.100'} justifyContent={'center'} alignItems={'center'}>
-      <H1 textAlign={'center'}>KM Khalid Saifullah</H1>
+    <Flex
+      width={"100%"}
+      height={"100px"}
+      bgColor={"gray.100"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <H1 textAlign={"center"}>KM Khalid Saifullah</H1>
     </Flex>
   );
 }
